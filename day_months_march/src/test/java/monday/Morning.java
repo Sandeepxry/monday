7ee7b097e6df00e137f85ecbd3baa77a24ff571a
USER <USER@USER-PC>
2021-03-15 13:58:39 +0530

    first

---
+++ day_months_march/src/test/java/monday/Morning.java
@@ -0,0 +1,20 @@
+package monday;
+
+import org.testng.annotations.Test;
+
+public class Morning {
+	
+	@Test
+	public void m1() {
+		System.out.println("5 am");
+	}
+	@Test
+	public void m2() {
+		System.out.println("6 am");
+	}
+	@Test
+	public void m3() {
+		System.out.println("7 am");
+	}
+
+}
---
+++ day_months_march/src/test/java/monday/Noon.java
@@ -0,0 +1,20 @@
+package monday;
+
+import org.testng.annotations.Test;
+
+public class Noon {
+	@Test
+	public void m4() {
+		System.out.println("5 am");
+	}
+	@Test
+	public void m5() {
+		System.out.println("6 am");
+	}
+	@Test
+	public void m6() {
+		System.out.println("7 am");
+	}
+
+
+}
